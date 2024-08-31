36b7280fdc25e7000137dbd7e5fcc92597cc07bd
kelejohn <132419716+kelejohn@users.noreply.github.com>
2024-08-30 21:19:37 -0400

    First message of assignment 1

---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment1/HelloWorld.java
@@ -0,0 +1,12 @@
+//Kelynn Johnson
+
+package org.howard.edu.lsp.assignment1;
+
+public class HelloWorld {
+
+	public static void main(String[] args) {
+		System.out.println("Hello World!!");	
+
+	}
+
+}
