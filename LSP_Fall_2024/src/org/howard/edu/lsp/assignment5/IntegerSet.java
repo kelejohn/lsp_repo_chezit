a734c5a85e6033aed418c1ba73ae033456bd5f72
kelejohn <132419716+kelejohn@users.noreply.github.com>
2024-10-23 13:15:02 -0400

    assignment 5

--- LSP_Fall_2024/src/org/howard/edu/lsp/assignment5/IntegerSet.java
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment5/IntegerSet.java
@@ -1,6 +1,7 @@
 package org.howard.edu.lsp.assignment5;
 
 import java.util.ArrayList;
+import java.util.Collections;
 import java.util.List;
 
 public class IntegerSet  {
@@ -16,49 +17,92 @@ public class IntegerSet  {
 		this.set = set;
 	}
 
-	// Clears the internal representation of the set. 5 pts.
-public void clear() {…};
+	// Clears the internal representation of the set.
+	public void clear() {
+		set.clear();
+	}
 
-// Returns the length of the set. 5 pts.
-public int length() {…}; // returns the length
+	// Returns the length of the set. 
+	public int length() {
+		return set.size();
+	}; 
 
-/*
-              * Returns true if the 2 sets are equal, false otherwise;
- * Two sets are equal if they contain all of the same values in ANY order.  This overrides
- * the equal method from the Object class. 10 pts.
-*/
-public boolean equals(Object o) {…}; 
-
-// Returns true if the set contains the value, otherwise false. 5 pts.
-public boolean contains(int value) {…};    
-// Returns the largest item in the set. 5 pts.
-public int largest()  {…}; 
-
-// Returns the smallest item in the set. 5 pts.
-public int smallest()  {…};
-
+	/*
+	 * Returns true if the 2 sets are equal, false otherwise;
+	 * Two sets are equal if they contain all of the same values in ANY order.  This overrides
+	 * the equal method from the Object class. 10 pts.
+	*/
+	public boolean equals(Object o) {
+		if (this == o) return true;
+		if (o == null || getClass() != o.getClass()) return false;
+		
+		IntegerSet intSet = (IntegerSet) o;
+		return set.containsAll(intSet.set) && intSet.set.containsAll(set);
+	}
+	
+	// Returns true if the set contains the value, otherwise false. 5 pts.
+	public boolean contains(int value) {
+		return set.contains(value);
+	}
+	
+	// Returns the largest item in the set. 5 pts.
+	public int largest()  {
+		return Collections.max(set);
+	}
+	
+	// Returns the smallest item in the set. 5 pts.
+	public int smallest()  {
+		return Collections.min(set);
+	}
+	
 	// Adds an item to the set or does nothing it already there. 5 pts.	
- 	public void add(int item) {…}; // adds item to the set or does nothing if it is in set
-
+	public void add(int item) {
+		if (!set.contains(item)) {
+			set.add(item);
+		}
+	}
+	
 	// Removes an item from the set or does nothing if not there. 5 pts.
-public void remove(int item) {…}; 
-
-// Set union. 11 pts.
-public void union(IntegerSet intSetb) {…};
-
-// Set intersection, all elements in s1 and s2. 12 pts.
-public void intersect(IntegerSet intSetb) {…}; 
-
-// Set difference, i.e., s1 –s2. 12 pts.
-public void diff(IntegerSet intSetb); // set difference, i.e. s1 - s2
-
-// Set complement, all elements not in s1. 11 pts.
-public void complement(IntegerSet intSetb) { …}
-
-// Returns true if the set is empty, false otherwise. 5 pts.
-boolean isEmpty(); 
-
-// Return String representation of your set.  This overrides the equal method from 
-// the Object class. 5 pts.
-public String toString() {…};	// return String representation of your set
-}
+	public void remove(int item) {
+		if (!set.contains(item)) {
+			set.remove(item);
+		}
+	} 
+	
+	// Set union. 11 pts.
+	public void union(IntegerSet intSetb) {
+		for (int item: intSetb.set) {
+			this.add(item);
+		}
+	}
+	
+	// Set intersection, all elements in s1 and s2. 12 pts.
+	public void intersect(IntegerSet intSetb) {
+		set.retainAll(intSetb.set);
+	}
+	
+	// Set difference, i.e., s1 –s2. 12 pts.
+	public void diff(IntegerSet intSetb){
+		set.removeAll(intSetb.set);
+	}
+	
+	// Set complement, all elements not in s1. 11 pts.
+	public void complement(IntegerSet intSetb) {
+		for (int item : intSetb.set) {
+			if (!this.set.contains(item)) {
+				this.add(item);
+			}
+		}
+	}
+	
+	// Returns true if the set is empty, false otherwise. 5 pts.
+	boolean isEmpty() {
+		return set.isEmpty();
+	}
+	
+	// Return String representation of your set.  This overrides the equal method from 
+	// the Object class. 5 pts.
+	public String toString() {
+		return set.toString();
+	}
+	}
---
+++ LSP_Fall_2024/src/org/howard/edu/lsp/assignment5/IntegerSetDriver.java
@@ -0,0 +1,28 @@
+package org.howard.edu.lsp.assignment5;
+
+public class IntegerSetDriver{
+	public static void main(String[] args) {
+		IntegerSet set1 = new IntegerSet();
+		set1.add(1);
+		set1.add(2);
+		set1.add(3);
+		
+		System.out.println("Value of Set1 is: " + set1.toString());
+		System.out.println("Smallest value in Set1 is: " + set1.smallest());
+		System.out.println("Largest value in Set1 is: " + set1.largest());
+		
+		IntegerSet set2 = new IntegerSet();
+		set2.add(3);
+		set2.add(4);
+		set2.add(5);
+		
+		System.out.println("Union of Set1 and Set2:");
+		System.out.println("Value of Set1 is: " + set1.toString());  // Before union
+		System.out.println("Value of Set2 is: " + set2.toString());  // Before union
+
+		set1.union(set2);  // union of set1 and set2
+		
+		System.out.println("Result of union of Set1 and Set2:");
+		System.out.println("Value of Set1 is: " + set1.toString());  // After union
+	}
+}
